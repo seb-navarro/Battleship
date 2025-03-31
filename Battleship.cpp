c957d46d832450b90d69c4812a74d0507c97feb1
seb-navarro <navarrosebastian154@gmail.com>
2025-03-31 13:07:10 +0100

    added function to get coordinate

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+string board[5][7] = {{"A", "O", "O", "O", "O", "O", "O"}, {"B", "O", "O", "O", "O", "O", "O"}, {"C", "O", "O", "O", "O", "O", "O"}, {"D", "O", "O", "O", "O", "O", "O"}, {" ", "1", "2", "3", "4", "5", "6"}};
 
-    string board[5][7] = {{"A", "O", "O", "O", "O", "O", "O"}, {"B", "O", "O", "O", "O", "O", "O"}, {"C", "O", "O", "O", "O", "O", "O"}, {"D", "O", "O", "O", "O", "O", "O"}, {" ", "1", "2", "3", "4", "5", "6"}};
 
+void show_board() {
     for (int a = 0; a < 5; a++) {
         for (int b = 0; b < 7; b++) {
             cout << board[a][b] << " ";
         }
         cout << endl;
     }
+}
+
+string get_coordinate() {
+    string coordinate;
+    cout << "Input Coordinate to Fire On";
+    cin >> coordinate;
+
+    return coordinate;
+}
+
+int main() {
+
+    show_board();
 
     return 0;
 }
